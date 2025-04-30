30ae4ab03d9112bbc805a643a8cb0905049ce83d
Angel Orozco <angelalfonsool20@gmail.com>
2025-04-29 21:54:28 -0600

    Configuracion inicial de las rutas con la depencia de Go_Router. Creacion de la ruta para HomeScreen, y posibles rutas futuras.

---
+++ lib/config/router.dart
@@ -0,0 +1,38 @@
+import 'package:go_router/go_router.dart';
+import 'package:tortilleria_chaly/screen/home_screen/home_screen.dart';
+
+final routerConfig = GoRouter(
+  initialLocation: HomeScreen.path,
+  routes: [
+    //HomeScreen
+    GoRoute(
+      path: HomeScreen.path,
+      name: HomeScreen.path,
+      builder: (context, state) => const HomeScreen(),
+    ),
+
+    /*//CreateClientScreen
+    GoRoute(
+      path: CreateClientScreen.path,
+      name: CreateClientScreen.path,
+      builder: (context, state) => CreateClientScreen(),
+    ),
+
+    //ListClientScreen
+    GoRoute(
+      path: ListClientScreen.path,
+      name: ListClientScreen.path,
+      builder: (context, state) => const ListClientScreen(),
+    ),
+
+    //ViewClientScreen
+    GoRoute(
+      path: "${ViewClientScreen.path}/:id",
+      name: ViewClientScreen.path,
+      builder: (context, state) {
+        final idClient = int.parse(state.pathParameters["id"]!);
+        return ViewClientScreen(idClient: idClient);
+      },
+    ),*/
+  ],
+);
