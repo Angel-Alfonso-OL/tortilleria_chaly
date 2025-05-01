c16ab9e73bf1d2fa4b653aafd479d3d22bc08ebe
Angel Orozco <angelalfonsool20@gmail.com>
2025-04-30 21:18:56 -0600

    Creacion de la pantalla CreateClientScreen.

---
+++ lib/presentation/screen/create_client_screen/create_client_screen.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tortilleria_chaly/config/colors.dart';
+import 'package:tortilleria_chaly/presentation/screen/widget/custom_text_form_field.dart';
+import 'package:tortilleria_chaly/presentation/screen/widget/input_validator.dart';
+import 'package:tortilleria_chaly/domain/entities/client/client.dart';
+import 'package:tortilleria_chaly/presentation/provider/client_providers/client_db_provider.dart';
+import 'package:tortilleria_chaly/presentation/provider/isar_conecction_provider.dart';
+import 'package:tortilleria_chaly/presentation/provider/client_providers/get_list_client_provider.dart';
+
+class CreateClientScreen extends ConsumerWidget {
+  static const path = "/CreateClientScreen";
+
+  CreateClientScreen({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  final _textControllerName = TextEditingController();
+  final _textControllerNickname = TextEditingController();
+  final _textControllerdauda = TextEditingController();
+
+  @override
+  Widget build(BuildContext context, ref) {
+    ref.watch(isarConnectionProvider);
+    final fecha = DateTime.now();
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "  REGISTRAR CLIENTE",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          ),
+          centerTitle: true,
+          leading: GestureDetector(
+            onTap: () {
+              context.pop();
+            },
+            child: const Icon(
+              Icons.close,
+              size: 28,
+            ),
+          ),
+          backgroundColor: darkBlue,
+          foregroundColor: white,
+        ),
+        body: Container(
+          height: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const SizedBox(height: 10),
+                  const Icon(Icons.account_circle, size: 130, color: darkGrey),
+                  const Divider(
+                    height: 2,
+                    color: darkGrey,
+                  ),
+                  const SizedBox(height: 30),
+                  TextFormField(
+                    enabled: false,
+                    style: const TextStyle(
+                        color: darkBlue,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16),
+                    initialValue:
+                        "${fecha.day} / ${fecha.month} / ${fecha.year}",
+                    textCapitalization: TextCapitalization.words,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: const InputDecoration(
+                      label: Text("FECHA DE REGISTRO"),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 30),
+                  CustomTextFormField(
+                    controller: _textControllerName,
+                    label: "NOMBRE",
+                    textInputType: TextInputType.name,
+                    funcionValidadora: funcionesValidadoras["text"]!,
+                  ),
+                  const SizedBox(height: 30),
+                  CustomTextFormField(
+                    controller: _textControllerNickname,
+                    label: "APODO",
+                    textInputType: TextInputType.name,
+                    funcionValidadora: funcionesValidadoras["text"]!,
+                  ),
+                  const SizedBox(height: 30),
+                  CustomTextFormField(
+                    controller: _textControllerdauda,
+                    label: "DEUDA",
+                    textInputType: TextInputType.number,
+                    funcionValidadora: funcionesValidadoras["number"]!,
+                  ),
+                  const SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () async {
+                      if (_formKey.currentState!.validate()) {
+                        await ref.read(clientDbProvider).createClient(Client(
+                            name: _textControllerName.value.text,
+                            nickName: _textControllerNickname.text,
+                            lastSale: DateTime.now(),
+                            lastPay: DateTime.now(),
+                            money: int.parse(_textControllerdauda.value.text)));
+                        ref.invalidate(getListClientProvider);
+                        context.pop();
+                      }
+                    },
+                    child: Container(
+                      height: 55,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: darkBlue,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "GUARDAR",
+                          style: TextStyle(
+                            color: white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () {
+                      context.pop();
+                    },
+                    child: Container(
+                      height: 55,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: darkGrey,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "CANCELAR",
+                          style: TextStyle(
+                            color: white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20,)
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
