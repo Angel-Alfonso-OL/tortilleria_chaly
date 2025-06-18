71f91eb11214db4f2b4bf41c958ab1d1c80cb056
Angel Orozco <angelalfonsool20@gmail.com>
2025-06-17 23:07:06 -0600

    Creando provider para acceder a LastSummary.

---
+++ lib/presentation/provider/last_summary_provider.dart
@@ -0,0 +1,60 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tortilleria_chaly/domain/entities/summary/summary.dart';
+import 'package:tortilleria_chaly/presentation/provider/summary_db_provider.dart';
+
+final lastSummaryProvider =
+    StateNotifierProvider<LastSummaryProvider, Summary>((ref) {
+  return LastSummaryProvider(ref);
+});
+
+class LastSummaryProvider extends StateNotifier<Summary> {
+  final Ref ref;
+
+  LastSummaryProvider(this.ref) : super(Summary.defaulSummary);
+
+  Future<void> updateLastSummary() async {
+    final listSumamry = await ref.read(summaryDbProvider).getAllSummary();
+    state = listSumamry.last;
+  }
+
+  Future<void> insertNewSummary(Summary newSummary) async{
+    await ref.read(summaryDbProvider).createSummary(newSummary);
+    await updateLastSummary();
+  }
+
+  Future<void> discoverLastSummary() async{
+     final a = await ref.read(summaryDbProvider).getAllSummary();
+    //Si la lista no esta vacia
+    if (a.isNotEmpty) {
+      // Si la ultima fecha es diferente a la actual
+      if (a.last.date.day != DateTime.now().day) {
+        await insertNewSummary(
+              Summary(
+                tortillasHechas: 0,
+                tortillasSobrantes: 0,
+                tortillasVendidas: 0,
+                tortillasVendidasEspeciales: 0,
+                tortillasVendidasTienda: 0,
+                totalFiados: 0,
+                totalPagados: 0,
+                date: DateTime.now(),
+              ),
+            );
+      }
+      //Si la lista esta vacia
+    } else {
+      await insertNewSummary(
+            Summary(
+              tortillasHechas: 0,
+              tortillasSobrantes: 0,
+              tortillasVendidas: 0,
+              tortillasVendidasEspeciales: 0,
+              tortillasVendidasTienda: 0,
+              totalFiados: 0,
+              totalPagados: 0,
+              date: DateTime.now(),
+            ),
+          );
+    }
+  }
+}
