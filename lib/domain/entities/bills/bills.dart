c793fd78545ba3041b131c182decabfee3ed8790
Angel Orozco <angelalfonsool20@gmail.com>
2025-06-30 21:08:22 -0600

    Actualizacion de la entidad de Bills.

--- lib/domain/entities/bills/bills.dart
+++ lib/domain/entities/bills/bills.dart
@@ -11,12 +11,39 @@ class Bills {
   final int luz;
 
   Bills(
-      {required this.billsId,
+      {this.billsId = Isar.autoIncrement,
       required this.date,
       required this.compras,
       required this.gas,
       required this.salario,
       required this.comida,
-      required this.luz}
+      required this.luz});
+
+  static Bills defaultBills = Bills(
+    date: DateTime.now(),
+    compras: 0,
+    gas: 0,
+    salario: 0,
+    comida: 0,
+    luz: 0,
   );
+
+  Bills copyWith(
+    DateTime? date,
+    int? compras,
+    int? gas,
+    int? comida,
+    int? luz,
+    int? salario,
+  ) {
+    return Bills(
+      billsId: billsId,
+      date: date ?? this.date,
+      compras: compras ?? this.compras,
+      gas: gas ?? this.gas,
+      salario: salario ?? this.salario,
+      comida: comida ?? this.comida,
+      luz: luz ?? this.luz,
+    );
+  }
 }
