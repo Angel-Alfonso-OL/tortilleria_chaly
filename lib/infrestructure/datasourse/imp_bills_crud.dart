350535eb830916252389fbdd41ace118904f2ebf
Angel Orozco <angelalfonsool20@gmail.com>
2025-07-05 13:18:18 -0600

    Implementacion de las clases BillsCRUD y RepositoryBills.

---
+++ lib/infrestructure/datasourse/imp_bills_crud.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:isar/isar.dart';
+import 'package:tortilleria_chaly/domain/datasourse/bills_crud.dart';
+import 'package:tortilleria_chaly/domain/entities/bills/bills.dart';
+
+class ImpBillsCRUD implements BillsCRUD{
+
+  final Isar isarConnectivity;
+
+  ImpBillsCRUD({required this.isarConnectivity});
+ 
+  @override
+  Future<void> createBills(Bills client) async{
+    await isarConnectivity.writeTxn(() async {
+      await isarConnectivity.bills.put(client);
+    });
+  }
+
+  @override
+  Future<void> delateBills(int id) async{
+     await isarConnectivity.writeTxn(() async {
+      isarConnectivity.bills.delete(id);
+    });
+  }
+
+  @override
+  Future<List<Bills>> getAllBills() async{
+    return isarConnectivity.bills.where().findAll();
+  }
+
+  @override
+  Future<Bills> getBillsById(int id) async{
+    return await isarConnectivity.bills.get(id) ?? Bills.defaultBills;
+  }
+
+  @override
+  Future<void> updateBills(Bills client) async{
+     await isarConnectivity.writeTxn(() async {
+      isarConnectivity.bills.put(client);
+    });
+  }
+
+}
---
+++ lib/infrestructure/repository/imp_repository_bills.dart
@@ -0,0 +1,34 @@
+import 'package:tortilleria_chaly/domain/datasourse/bills_crud.dart';
+import 'package:tortilleria_chaly/domain/entities/bills/bills.dart';
+import 'package:tortilleria_chaly/domain/repository/respository_bills.dart';
+
+class ImpRepositoryBills implements RespositoryBills {
+  final BillsCRUD dataBase;
+
+  ImpRepositoryBills({required this.dataBase});
+
+  @override
+  Future<void> createBills(Bills bills) async {
+    await dataBase.createBills(bills);
+  }
+
+  @override
+  Future<void> delateBills(int id) async {
+    await dataBase.delateBills(id);
+  }
+
+  @override
+  Future<List<Bills>> getAllBills() async {
+    return dataBase.getAllBills();
+  }
+
+  @override
+  Future<Bills> getBillsById(int id) async {
+    return await dataBase.getBillsById(id);
+  }
+
+  @override
+  Future<void> updateBills(Bills bills) async {
+    await dataBase.updateBills(bills);
+  }
+}
